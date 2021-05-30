4d638f0831021d861c28717a787c480e9709d44c
Brent Lanteigne <brent.lanteigne@toronto.ca>
2021-05-30 14:58:11 +0000

    Initial

---
+++ src/test/java/ca/toronto/csg/recapchademo/RecapchaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.toronto.csg.recapchademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapchaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
